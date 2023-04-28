98de9ea73bbd3901593b6630c0240ada40458f27
fbonilla_ <alejandrovelez1234567@gmail.com>
2023-04-28 19:02:53 +0200

    Implementación banner

---
+++ js/index.js
@@ -0,0 +1,21 @@
+window.onload = function inicio() {
+    
+    bannerInicio();
+
+
+
+
+
+}
+
+//Create banner
+function bannerInicio() {
+
+    var myModal = new bootstrap.Modal(document.getElementById('myModal'), {})
+    myModal.toggle()
+
+
+
+
+}
+

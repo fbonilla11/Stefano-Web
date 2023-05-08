785e83c57598c4a671db96981928e50bd84cd2bc
fbonilla_ <alejandrovelez1234567@gmail.com>
2023-05-08 15:08:06 +0200

    -

--- js/index.js
+++ js/index.js
@@ -1,21 +1,50 @@
+
+//Onload function
 window.onload = function inicio() {
-    
-    bannerInicio();
+
+    homeModal();
+
+}
+
+//Scroll effects
+window.addEventListener("scroll", reveal);
+
+//Create modal
+function homeModal() {
 
 
+    try {
+        setTimeout(() => {
+
+            var myModal = new bootstrap.Modal(document.getElementById('myModal'), {})
+            myModal.toggle();
+            console.log("Modal lanzado");
+
+        }, 5000); 
+
+     } catch (error) {
+        reject("Error, no se ha podido lanzar el modal " + error);
+    }
 
 
 
 }
 
-//Create banner
-function bannerInicio() {
-
-    var myModal = new bootstrap.Modal(document.getElementById('myModal'), {})
-    myModal.toggle()
-
-
-
-
-}
+//Create scroll effect
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 120;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
 
